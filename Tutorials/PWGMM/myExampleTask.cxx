564ff177bc373caaade57b5a82db736950f184d5
Paul Veen <paul.veen@yahoo.com>
2024-10-17 13:37:46 +0200

    Test: tutorial O2 pushed to fork

--- Tutorials/PWGMM/myExampleTask.cxx
+++ Tutorials/PWGMM/myExampleTask.cxx
@@ -15,6 +15,7 @@
 
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
+#include "Common/DataModel/TrackSelectionTables.h"
 
 using namespace o2;
 using namespace o2::framework;
@@ -23,19 +24,29 @@ struct myExampleTask {
   // Histogram registry: an object to hold your histograms
   HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
+  Configurable<int> nBinsPt{"nBinsPt", 100, "N bins in pT histo"};
+
   void init(InitContext const&)
   {
     // define axes you want to use
+    const AxisSpec axisCounter{1, 0, +1, ""};
     const AxisSpec axisEta{30, -1.5, +1.5, "#eta"};
+    const AxisSpec axisPt{nBinsPt, 0, 10, "p_{T}"};
 
     // create histograms
-    histos.add("etaHistogram", "etaHistogram", kTH1F, {axisEta});
+    histos.add("eventCounterHistogram", "eventCounterHistogram", kTH1F, {axisCounter});
+    histos.add("eta1Histogram", "eta1Histogram", kTH1F, {axisEta});
+    histos.add("ptHistogram", "ptHistogram", kTH1F, {axisPt});
   }
 
-  void process(aod::TracksIU const& tracks)
+  void process(aod::Collision const& collision, soa::Join<aod::Tracks, aod::TracksExtra, aod::TracksDCA> const& tracks)
   {
+    histos.fill(HIST("eventCounterHistogram"), 0.5);
     for (auto& track : tracks) {
-      histos.fill(HIST("etaHistogram"), track.eta());
+      if (track.tpcNClsCrossedRows() < 70) continue;
+      if (fabs(track.dcaXY()) > 0.2) continue;
+      histos.fill(HIST("eta1Histogram"), track.eta());
+      histos.fill(HIST("ptHistogram"), track.pt());
     }
   }
 };
